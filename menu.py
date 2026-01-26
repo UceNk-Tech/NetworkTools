3318031238ed0820946aa2b5a90592ea78c0b73c
UceNk-Tech <ucenkzr@gmail.com>
2026-01-26 14:50:35 +0800

    Update menu.py

--- menu.py
+++ menu.py
@@ -995,7 +995,7 @@ def check_optical_power_fast():
     port = input(f"{WHITE}Masukkan Port (contoh 1/2/1): {RESET}").strip()
     onu_id = input(f"{WHITE}NO ONU: {RESET}").strip()
     
-    print(f"\n{CYAN}[*] Mohon tunggu, sedang mengambil data redaman...{RESET}")
+    print(f"\n{CYAN}[*] Mohon tunggu, sedang berkomunikasi dengan ONU...{RESET}")
 
     if brand == 'fiberhome':
         cmds = ["terminal length 0", f"show onu optical-power {port} {onu_id}"]
@@ -1008,7 +1008,7 @@ def check_optical_power_fast():
     output = telnet_olt_execute(creds, cmds)
     
     print(f"\n{WHITE}HASIL DIAGNOSA ONU {onu_id} @ PORT {port}:{RESET}")
-    print(f"{MAGENTA}-------------------------------------------------------------------------------{RESET}")
+    print(f"{MAGENTA}---------------------------------------------------------------------------------------------------------------------------------------------{RESET}")
     
     if output:
         # --- PROSES PEMBERSIHAN OUTPUT ---
@@ -1025,7 +1025,7 @@ def check_optical_power_fast():
         
         # Tampilkan tabel yang sudah bersih
         print(f"{YELLOW}{clean_output}{RESET}")
-        print(f"{MAGENTA}-------------------------------------------------------------------------------{RESET}")
+        print(f"{MAGENTA}-------------------------------------------------------------------------------------------------------------------------------------------{RESET}")
 
         # --- LOGIKA PENGAMBILAN RX ONU (BARIS DOWN) ---
         rx_val = None
@@ -1054,7 +1054,7 @@ def check_optical_power_fast():
     else:
         print(f"{RED}[!] Gagal koneksi ke OLT.{RESET}")
 
-    print(f"{MAGENTA}-------------------------------------------------------------------------------{RESET}")
+    print(f"{MAGENTA}---------------------------------------------------------------------------------------------------------------------------------------------{RESET}")
 
 
 def port_vlan(): 

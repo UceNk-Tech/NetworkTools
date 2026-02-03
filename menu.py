63006d7675dbfd557d62c1bdd175ebb320c84113
UceNk-Tech <ucenkzr@gmail.com>
2026-02-03 12:25:23 +0800

    Refactor speedtest execution in menu.py
    Updated speedtest execution method to use 'speedtest-cli --simple' directly.

--- menu.py
+++ menu.py
@@ -1411,15 +1411,8 @@ def main():
         elif c == '17': traffic_report_pon()
         elif c == '18': auto_audit_olt()    
         elif c == '19':
-            print(f"\n{CYAN}--- Menjalankan Speedtest (Stay on Screen) ---{RESET}")
-            # Versi pkg biasanya tidak butuh --accept-license, 
-            # tapi kita tambahkan try-except agar aman
-            try:
-                # Coba jalankan versi binary
-                os.system("speedtest")
-            except:
-                # Jika gagal, balik ke standar
-                os.system("speedtest-cli --simple")
+            print(f"\n{CYAN}--- Menjalankan Speedtest ---{RESET}")
+            os.system("speedtest-cli --simple")
         elif c == '20': nmap_scan_tool()
         elif c == '21': mac_lookup_tool()
         elif c == '22': port_scanner_tool()
